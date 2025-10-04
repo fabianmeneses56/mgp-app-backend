e34a3c1a4cd04d9417c7146c624c159a83f68713
fabianmeneses56 <fabianmeneses56@hotmail.com>
2025-10-04 17:01:39 -0500

    feat: create user implementation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,67 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    // private readonly jwtService: JwtService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+
+      await this.userRepository.save(user);
+      delete user.password; // quitar la contraseña de los datos de la tabla
+
+      return {
+        ...user,
+        // token: this.getJwtToken({ id: user.id }),
+      };
+      // TODO: Retornar el JWT de acceso
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  // async login(loginUserDto: LoginUserDto) {
+  // const { password, email } = loginUserDto;
+  // const user = await this.userRepository.findOne({
+  //   where: { email },
+  //   select: { email: true, password: true, id: true }, //! OJO! solo recibo lo que pongo en el select
+  // });
+  // if (!user)
+  //   throw new UnauthorizedException('Credentials are not valid (email)');
+  // if (!bcrypt.compareSync(password, user.password))
+  //   throw new UnauthorizedException('Credentials are not valid (password)');
+  // return {
+  //   ...user,
+  //   token: this.getJwtToken({ id: user.id }),
+  // };
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+  private handleDBErrors(error: any): never {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+    console.log(error);
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    throw new InternalServerErrorException('Please check server logs');
   }
 }
